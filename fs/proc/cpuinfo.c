3168ae606e95ff99b79ceeb36069789ff765bd09
root <root@d68p1.aus.stglabs.ibm.com>
2020-10-28 14:45:18 -0500

    Teste de ipi

--- fs/proc/cpuinfo.c
+++ fs/proc/cpuinfo.c
@@ -5,13 +5,35 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#include <linux/smp.h>
+#include <linux/atomic.h>
+
+static int first = 0;
+#warning "DEBUGGING smp_call_function v2"
 __weak void arch_freq_prepare_all(void)
 {
+}
+
+static void justsum(void *buf)
+{
+	atomic_t *a = buf;
+	atomic_add(smp_processor_id() + 1, a);
+	if (first++ == 0)
+		pr_err("leobras: debugging smp_call_function\n");
+
 }
 
 extern const struct seq_operations cpuinfo_op;
 static int cpuinfo_open(struct inode *inode, struct file *file)
 {
+	atomic_t a;
+	int b;
+
+	smp_call_function(justsum, &a, true);
+	b = atomic_read(&a) + smp_processor_id() + 1;
+	if (b != (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8))
+		pr_err("leobras: debugging smp_call_funct only ran in %d cpus\n", b);
+
 	arch_freq_prepare_all();
 	return seq_open(file, &cpuinfo_op);
 }
